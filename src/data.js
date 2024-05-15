bf7114495a0f81c9e29b83bc414c2db962ff9a49
weigucheng <weigucheng@i-hc.cn>
2024-05-15 18:06:52 +0800

    修改代码

--- src/data.js
+++ src/data.js
@@ -7,8 +7,11 @@ const solarTermsNameList = [];
 const cnNamesList = [];
 const cnNamesMapList = {};
 
+const solarTermMap = {};
+
 const stColorMapList = []; // 按节气分的列表（index）
 const colorsMap = {}; // 所有颜色的直接 map
+
 const namesMap = {}; // 中 => 英
 const cnNamesMap = {}; // 英 => 中
 
@@ -19,6 +22,19 @@ for (const item of data) {
     solarTermsNameList.push(name);
     const nameList = [];
     const stColorMap = {};
+
+    const solarTermObj = {
+        getColorByName,
+        getAllColors () {
+            return getColorMapBySolarTerm(color.name);
+        }
+    };
+
+    solarTermMap[name] = solarTermObj;
+
+    cnNamesMap[name] = cnName;
+    namesMap[cnName] = name;
+
     for (const color of items) {
         const { rgb: [ r, g, b ], cmyk: [ c, m, y, k], } = color.value;
         cnNamesList.push(color.cnName);
@@ -53,37 +69,44 @@ for (const item of data) {
     output[name] = outMap;
 }
 
+function getName (name) {
+    let colorName = namesMap[name];
+    if (!colorName) {
+        colorName = name;
+    }
+    return colorName;
+}
+
+function getColorByName (name) {
+    const colorName = getName(name);
+    const color = colorsMap[colorName] || null;
+    return color;
+}
+
+function getColorMapBySolarTerm (input) {
+    if (isNaN(input)) {
+        if (solarTermsCnNameList.indexOf(input) < 0) { // 英文
+            input = solarTermsNameList.indexOf(input);
+        } else { // 中文
+            input = solarTermsCnNameList.indexOf(input);
+        }
+    }
+    const obj = stColorMapList[input];
+    return obj || null;
+}
+
 exports = module.exports = {
-    getColorByName (name) {
-        let colorName = namesMap[name];
-        if (!colorName) {
-            colorName = name;
-        }
-        return colorsMap[colorName];
-    },
-    getColorMapBySolarTerm (input) {
-        if (isNaN(input)) {
-            if (solarTermsCnNameList.indexOf(input) < 0) { // 英文
-                input = solarTermsNameList.indexOf(input);
-            } else { // 中文
-                input = solarTermsCnNameList.indexOf(input);
-            }
-        }
-        return stColorMapList[input];
-    },
-    getAllColorCnNameList () {
-        return [].concat(cnNamesList);
-    },
+    getColorByName,
+    getColorMapBySolarTerm,
     getAllSolarTermCnNameList () {
         return [].concat(solarTermsCnNameList);
     },
     getAllSolarTermNameList () {
         return [].concat(solarTermsNameList);
     },
-    getColorNameByCnName (cnName) {
-        return namesMap[cnName];
-    },
-    getColorCnNameByName (name) {
-        return cnNamesMap[name];
+    getSolarTermByName (name) {
+        const colorName = getName(name);
+        return solarTermMap[colorName];
+
     }
 };
