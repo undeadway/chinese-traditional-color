274b8d750874d935c0347616d99ad043f65009be
hzwaygc <hzwaygc@gmail.com>
2024-05-19 22:27:20 +0800

    优化代码并加入注释

--- src/methods.js
+++ src/methods.js
@@ -15,18 +15,15 @@ exports = module.exports = {
 		return `device-cmyk(${c}% ${m}% ${y}% ${k}% / 1, rgb(${r} ${g} ${b}))`;
 	},
 	calcBrightness (r, g, b) {
+		// 明度计算公式请参阅 doc/Brightness_Calculation_Formula.svg
 		r = Math.pow(r / 255, 2.2);
+		g = Math.pow((g * 1.5 / 255), 2.2);
+		b = Math.pow((b * 0.6 / 255), 2.2);
 
-		g = g / 255 * 1.5;
-		g = Math.pow(g, 2.2);
-
-		b = b / 255 * 0.6;
-		b = Math.pow(b, 2.2);
-
-		const molecule = r + g + b;
-		let denominator = 1;
-		denominator += Math.pow(1.5, 2.2);
-		denominator += Math.pow(0.6, 2.2);
+		const molecule = r + g + b; // 分子
+		const denominator = 1 // 分母
+							+ Math.pow(1.5, 2.2)
+							+ Math.pow(0.6, 2.2);
 
 		let value = molecule / denominator;
 
