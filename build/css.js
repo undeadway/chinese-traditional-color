a35ebc0f27c741af8a5810e56f2438f05f32cc5d
weigucheng <weigucheng@i-hc.cn>
2024-05-22 18:30:59 +0800

    修改编译 css

--- build/css.js
+++ build/css.js
@@ -5,6 +5,7 @@ const data = require("./../res/data.json");
 const sass = require("sass");
 
 const vals = [];
+const exportsVals = [];
 
 for (const item of data) {
 	const { items, name } = item;
@@ -12,20 +13,20 @@ for (const item of data) {
 	for (const color of items) {
 		const rgb = methods.rgbToHex(... color.value.rgb);
 		const cmyk = methods.createCmyk(color.value);
-		const cssName = `$CTC_${name}_${color.name}`;
+		const cssName = `CTC_${name}_${color.name}`;
 
-		const rgbVal = `${cssName}:#${rgb};`;
-		const cmykVal = `${cssName}-cmyk:${cmyk};`;
 
 		vals.push(`/* 颜色：${color.cnName} */`);
-		vals.push(rgbVal);
-		vals.push(cmykVal);
+		exportsVals.push(`	${cssName}: #${rgb};`);
+		exportsVals.push(`	${cssName}-cmyk: ${cmyk};`);
 	}
 }
 
-const file = `${vals.join("\r\n")}`;
-const fileName = `${__dirname}/../dist/chinese-traditional-color.scss`;
+const file = `:export {
+${exportsVals.join("\r\n")}
+}`;
+const fileName = `${__dirname}/../dist/chinese-traditional-color.css`;
 fs.writeFileSync(fileName, file);
 
-const result = sass.compileString(input);
-console.log(result.css);
+// const result = sass.compileString(input);
+// console.log(result.css);
