67ade0051497880b654c3eec260338f271d78a63
weigucheng <weigucheng@i-hc.cn>
2024-05-22 17:29:48 +0800

    修改把 css 输出为 scss

--- build/css.js
+++ build/css.js
@@ -6,23 +6,21 @@ const vals = [];
 
 for (const item of data) {
 	const { items, name } = item;
-	vals.push(`	/* 节气：${item.cnName} */`);
+	vals.push(`/* 节气：${item.cnName} */`);
 	for (const color of items) {
 		const rgb = methods.rgbToHex(... color.value.rgb);
 		const cmyk = methods.createCmyk(color.value);
-		const cssName = `	--CTC_${name}_${color.name}`;
+		const cssName = `$CTC_${name}_${color.name}`;
 
 		const rgbVal = `${cssName}:#${rgb};`;
 		const cmykVal = `${cssName}-cmyk:${cmyk};`;
 
-		vals.push(`	/* 颜色：${color.cnName} */`);
+		vals.push(`/* 颜色：${color.cnName} */`);
 		vals.push(rgbVal);
 		vals.push(cmykVal);
 	}
 }
 
-const file = `:root {
-${vals.join("\r\n")}
-}`;
+const file = `${vals.join("\r\n")}`;
 
-fs.writeFileSync(`${__dirname}/../dist/chinese-traditional-color.css`, file);
+fs.writeFileSync(`${__dirname}/../dist/chinese-traditional-color.scss`, file);
